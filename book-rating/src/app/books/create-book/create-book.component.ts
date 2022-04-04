8e5074e85e5fd29c8c159c18ae6af3b6955d4c83
Johannes Hoppe <johannes.hoppe@haushoppe-its.de>
2022-04-04 13:48:52 +0200

    forms: basic html without logic

---
+++ book-rating/src/app/books/create-book/create-book.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'br-create-book',
+  templateUrl: './create-book.component.html',
+  styleUrls: ['./create-book.component.scss']
+})
+export class CreateBookComponent {
+
+  bookForm = new FormGroup({
+    isbn: new FormControl('', [Validators.required, Validators.minLength(3)]),
+    title: new FormControl('', Validators.required),
+    description: new FormControl()
+  });
+}
