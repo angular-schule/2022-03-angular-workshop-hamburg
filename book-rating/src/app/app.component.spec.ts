93dac61a08d411b9978a18eb6adec9e66441bc50
Johannes Hoppe <johannes.hoppe@haushoppe-its.de>
2022-04-04 10:31:41 +0200

    + Shallow Unit-Test

--- book-rating/src/app/app.component.spec.ts
+++ book-rating/src/app/app.component.spec.ts
@@ -1,6 +1,8 @@
+import { NO_ERRORS_SCHEMA } from '@angular/core';
 import { TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './books/dashboard/dashboard.component';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
@@ -9,8 +11,10 @@ describe('AppComponent', () => {
         RouterTestingModule
       ],
       declarations: [
-        AppComponent
+        AppComponent,
+        // DashboardComponent // Integrations-Test
       ],
+      schemas: [NO_ERRORS_SCHEMA] // Shallow Unit-Test
     }).compileComponents();
   });
 
