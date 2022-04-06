1d7a4ef290e37e405ddb6658f140b278039b44e3
Johannes Hoppe <johannes.hoppe@haushoppe-its.de>
2022-04-06 13:55:49 +0200

    // shareReplay(1)

--- book-rating/src/app/books/book-details/book-details.component.ts
+++ book-rating/src/app/books/book-details/book-details.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { concatMap, map, mergeMap, switchMap } from 'rxjs';
+import { concatMap, map, mergeMap, shareReplay, switchMap } from 'rxjs';
 import { BookStoreService } from '../shared/book-store.service';
 
 @Component({
@@ -13,7 +13,8 @@ export class BookDetailsComponent {
 
   book$ = this.route.paramMap.pipe(
     map(paramMap => paramMap.get('isbn')),
-    switchMap(isbn => this.bs.getSingleBook(isbn!))
+    switchMap(isbn => this.bs.getSingleBook(isbn!)),
+    // shareReplay(1)
   );
 
 
