c6b64cf79814eba09f14ba4d6cbedec3eeb30ae2
Johannes Hoppe <johannes.hoppe@haushoppe-its.de>
2022-04-06 14:14:34 +0200

    rxjs: catchError final

--- book-rating/src/app/books/book-details/book-details.component.ts
+++ book-rating/src/app/books/book-details/book-details.component.ts
@@ -1,7 +1,7 @@
 import { HttpErrorResponse } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { catchError, concatMap, map, mergeMap, of, shareReplay, switchMap } from 'rxjs';
+import { catchError, concatMap, map, mergeMap, of, retry, shareReplay, switchMap } from 'rxjs';
 import { BookStoreService } from '../shared/book-store.service';
 
 @Component({
@@ -15,6 +15,7 @@ export class BookDetailsComponent {
   book$ = this.route.paramMap.pipe(
     map(paramMap => paramMap.get('isbn')),
     switchMap(isbn => this.bs.getSingleBook(isbn!).pipe(
+      retry(3),
       catchError((err: HttpErrorResponse) => of({
         title: 'FEHLER',
         description: err.message
